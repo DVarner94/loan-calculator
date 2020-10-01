a840693ffc9e6a44551cbe9118efd92b890522b7
Dominic Varner <dommy@MLBs-Lappy.local>
2020-10-01 09:47:54 -0700

    event listeners and calculating results

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('hello')
+// listen for submit
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResults);
+
+// calculating results
+function calculateResults(e) {
+    
+    // ui vars
+    const amount = document.getElementById('amount')
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const MonthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value)/100/12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+
+    // calculate monthly payments
+    const x = Math.pow(1 +)
+
+
+    e.preventDefault();
+}
